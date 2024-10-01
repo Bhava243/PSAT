1339b81d1621e1f619e8e3923f0161bbf10da6a9
Bhava243 <bhavadarinimahendran@gmail.com>
2024-10-01 09:33:20 +0530

    Update while loop.py

--- loop.py/while loop.py
+++ loop.py/while loop.py
@@ -1,7 +1,7 @@
 count = 1
 
-while(count<=3):
+while(count <=5):
+    if count==3:
+        continue
     print("Hello World")
-    count = count +1
-else:
-    print("loop exited")    
+    count = count +1  
