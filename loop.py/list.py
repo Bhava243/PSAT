fa2a07d3520ebdf39994ff3ac941d649afc185b0
Bhava243 <bhavadarinimahendran@gmail.com>
2024-10-01 15:08:43 +0530

    Update list.py

--- loop.py/list.py
+++ loop.py/list.py
@@ -4,3 +4,17 @@ print(x[1])
 print(x[3])
 
 print(len(x))
+
+li = [1,2,'Hi',2.3]
+print(li)
+
+marks = []
+marks.append(5)
+marks.append(4)
+marks.append(6)
+marks.append((7,4))
+marks.append('1')
+marks.append([8,9])
+new_marks=marks
+marks.reverse()
+print(marks)
